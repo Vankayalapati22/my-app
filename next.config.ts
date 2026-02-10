8509f9625c4fccc03b1d4fa7f7c973dc1c36200e
Vankayalapati22 <balaraju.vankayalapati@winfosolutions.com>
2026-02-10 19:06:38 +0530

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */ 
+  /* config options here */
   output: "export",
-   basePath: "/my-app",
+  basePath: "/my-app",
   assetPrefix: "/my-app/",
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
